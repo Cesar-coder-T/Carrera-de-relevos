496cc5f442477cf713fb13c1e9ef853e9c40899d
César Téllez <carodrigueztellez@ucundinamarca.edu.co>
2021-04-10 12:35:06 -0500

    Subiendo proyecto

---
+++ src/main/java/co/edu/unicundi/carreraderelevos/controlador/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.carreraderelevos.controlador;
+
+import co.edu.unicundi.carreraderelevos.vista.Vista;
+
+/**
+ *
+ * @author cesar
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+    }
+}
